9cab57316c59ac92cc051785234b90c0dedefb0a
Onyiriuka Justin <richardjustin500@gmail.com>
2025-04-16 16:57:06 +0100

    local dev env added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 import app from "@/app.js";
-import { ENV } from "@/configs/env-config.js";
-import * as pino from "@/utils/logger.js";
-import { connectToDb } from "@/configs/mongodb.js";
-
-import { serve } from "@hono/node-server";
 import { logger } from "hono/logger";
 import { cors } from "hono/cors";
 import serveEmojiFavicon from "stoker/middlewares/serve-emoji-favicon";
@@ -28,12 +23,3 @@ app.notFound((c) => {
 
 app.onError(onError);
 
-const serverConfig = {
-  fetch: app.fetch,
-  port: ENV.PORT,
-};
-
-serve(serverConfig, async (info) => {
-  pino.default.info(`✅ Server is running on http://localhost:${info.port}`);
-  await connectToDb();
-});
