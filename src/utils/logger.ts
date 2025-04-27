2aa36bed587a9da148ea666ef2ef662e6fbc3ff6
Onyiriuka Justin <richardjustin500@gmail.com>
2025-04-27 15:34:41 +0100

    logger config modified

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,27 +1,30 @@
 import pino from "pino";
+import "dotenv/config";
 
-const logger = pino.default(
-  process.env.ENV || "development" === "development"
-    ? {
-        transport: {
-          target: "pino-pretty",
-          options: {
-            colorize: true,
-            translateTime: true,
-            ignore: "pid,hostname",
-            hideObject: false,
-            supportEmoji: true,
-          },
-        },
-      }
-    : {
-        formatters: {
-          level(label) {
-            return { level: label };
-          },
-        },
-        timestamp: pino.stdTimeFunctions.isoTime,
-      }
-);
+const isDev = process.env.ENV === "development";
+
+const logger = pino.default({
+  ...(!isDev && {
+    formatters: {
+      level(label) {
+        return { level: label };
+      },
+    },
+    timestamp: pino.stdTimeFunctions.isoTime,
+  }),
+
+  ...(isDev && {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        translateTime: true,
+        ignore: "pid,hostname",
+        hideObject: false,
+        supportEmoji: true,
+      },
+    },
+  }),
+});
 
 export default logger;
