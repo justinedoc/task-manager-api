1a05c4cf77cacdb4b97894b968dba9ae4ce2daca
Onyiriuka Justin <richardjustin500@gmail.com>
2025-05-12 10:11:18 +0100

    super admin added

---
+++ src/utils/super-admin.ts
@@ -0,0 +1,34 @@
+import { ENV } from "@/configs/env-config.js";
+import adminService from "@/services/admin-services.js";
+
+/* eslint-disable no-console */
+export async function createSuperAdmin() {
+  try {
+    const adminData = {
+      firstname: "System",
+      lastname: "Superadmin",
+      email: ENV.SUPERADMIN_EMAIL,
+      password: ENV.SUPERADMIN_PASS,
+    };
+
+    const adminExists = await adminService.exists(adminData.email);
+
+    if (adminExists) {
+      console.log("SUPERADMIN HAS ALREADY BEEN CREATED");
+      return;
+    }
+
+    const superadmin = await adminService.create(adminData);
+
+    console.log("Admin created successfully: \n");
+    console.log("----------------------------------");
+    console.log(`Email: ${superadmin.email}`);
+    console.log("----------------------------------");
+  } catch (error) {
+    console.error("\nFailed to create superadmin:");
+    console.error("----------------------------------");
+    console.error(error);
+    console.error("----------------------------------");
+    process.exit(1);
+  }
+}
